8900ab5ebbeeb383e6759125baebf1cf92d0364a
“AnonnaGH” <anonnaferdosh193@gmail.com>
2023-06-07 00:34:45 +0600

    create routes

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,17 @@ function App() {
 
 
   return (
-    <>
-      <h1>App</h1>
 
+    <>
+      <button className="btn">Button</button>
+      <button className="btn btn-primary">Button</button>
+      <button className="btn btn-secondary">Button</button>
+      <button className="btn btn-accent">Button</button>
+      <button className="btn btn-ghost">Button</button>
+      <button className="btn btn-link">Button</button>
     </>
+
+
   )
 }
 
