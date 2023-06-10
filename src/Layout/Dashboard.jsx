a6bb8996cb42ac0325f54a176ca24723307b46c7
“AnonnaGH” <anonnaferdosh193@gmail.com>
2023-06-10 20:46:03 +0600

    instructor and admin dashboard

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -1,9 +1,14 @@
 import { FaBook, FaHome, FaUsers, FaWallet } from "react-icons/fa";
 import { NavLink, Outlet } from 'react-router-dom';
+import useAdmin from "../hooks/useAdmin";
+import useInstructor from "../hooks/useInstructor";
 
 const Dashboard = () => {
 
-    const isAdmin = true;
+    // const isAdmin = true;
+    const [isAdmin] = useAdmin();
+    const [isInstructor] = useInstructor();
+    const isUser = !isAdmin && !isInstructor;
     return (
         <div className="drawer lg:drawer-open">
             <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
@@ -23,8 +28,8 @@ const Dashboard = () => {
                     {/* Sidebar content here */}
 
 
-                    {
-                        isAdmin ? <>
+                    {isAdmin && (
+                        <>
                             <li>
                                 <NavLink to="/dashboard/home">
                                     <FaHome /> Admin Home
@@ -46,20 +51,54 @@ const Dashboard = () => {
                                     <FaUsers /> All Users
                                 </NavLink>
                             </li>
-                        </> : <>
+                        </>
+                    )}
+
+                    {isInstructor && (
+                        <>
                             <li>
-                                <NavLink to="/">
-                                    <FaHome /> Home
+                                <NavLink to="/dashboard/home">
+                                    <FaHome /> Instructor Home
                                 </NavLink>
                             </li>
                             <li>
-                                <NavLink to="/classes"> All Classes</NavLink>
+                                <NavLink to="/dashboard/addclass">
+                                    Add new class
+                                </NavLink>
                             </li>
                             <li>
-                                <NavLink to="/classes">Enroll classes</NavLink>
+                                <NavLink to="/dashboard/manageclasses">
+                                    Manage Classes
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/dashboard/managebooking">
+                                    Manage Booking
+                                </NavLink>
                             </li>
                         </>
-                    }
+                    )}
+
+                    {isUser && (
+                        <>
+                            <li>
+                                <NavLink to="/dashboard/home">
+                                    <FaHome /> User Home
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/dashboard/mybookedclasses">
+                                    My booked classes
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/dashboard/myenrolledclasses">
+                                    My Enrolled Classes
+                                </NavLink>
+                            </li>
+                        </>
+                    )}
+
 
 
                     <div className="divider"></div>
